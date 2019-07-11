35d8fdb4457df52edcef89fb68280edbbae114ec
Evelyn <evelyn@ipsoft.es>
2019-07-11 20:10:29 +0100

    Environment setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-evelyn',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'My Angular App';
+  subTitle = 'Evelyn B. C.';
+
 }
